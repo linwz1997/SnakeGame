678df576e11c26d6ccec04be8eb79d4b7096a004
linwz860328 <lin860328@gmail.com>
2023-09-23 17:12:31 +0800

    Add: Use the srand() function to ensure that the fruit's position is not consistently the same at the beginning each time.

--- snake_game.cpp
+++ snake_game.cpp
@@ -32,8 +32,9 @@ void init(void)
     dir = RIGHT; //Set initial movement as RIGHT
     x = width / 2;
     y = height / 2;
-    fruitX = rand() % width;
-    fruitY = rand() % height;
+    srand((unsigned) time(0)); // To prevent from the pos always the same in everytime
+    fruitX = rand() % width + 1;
+    fruitY = rand() % height + 1;
     score = 0;
 }
 
